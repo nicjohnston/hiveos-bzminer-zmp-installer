41dfa28b75c15c63cf67217b523458683d00380d
Nicholas Johnston <nicholasjohnstoncode@gmail.com>
2022-09-11 21:57:39 -0600

    disable checks and miner stop for testing

--- install.sh
+++ install.sh
@@ -11,44 +11,45 @@ else
 	echo "Please provide your zil address as the first argument";
 	exit 1;
 fi
+echo $zilWallet
 exit 1;
 
-#check to make sure bzminer is running and installed
-#is bzminer configed and started?
-miner status | grep -q bzminer
-checkMiner=$?
-if [[ checkMiner == 1 ]]; then
-	echo "bzminer has not been started.  Make sure the flightsheet is configured to run bzminer 10.0.4 and start the miner ('miner start')";
-fi
-
-#is the executable installed and running
-checkInstalled=1
-if [ -f "$workDir/bzminer" ]; then
-	ps -ef | grep -q "[0-9] $workDir/bzminer";
-	checkInstalled=$?;
-fi
-if [[ $checkInstalled == 1 ]]; then
-	echo "the proper version of bzminer is not installed and running, make sure the flightsheet is set to 10.0.4 and use 'miner log' to check the installation process";
-fi
-
-#does the log file show the miner has started?
-grep -q "Working Directory: $workDir" /var/log/miner/bzminer/miner.log
-checkRunning=$?
-if [[ $checkRunning == 1 ]]; then
-	echo "bzminer doesn't seem to be running";
-fi
-
-if [[ $checkRunning == 1 || $checkInstalled == 1 || checkMiner == 1 ]]; then
-	echo "Try again goofus :-)";
-	exit 1;
-fi
-
-echo "Initial checks good, proceeding"
-
-#stop miner to prevent loosing mods to config.txt
-if ! miner stop ; then
-	echo "Failed to stop miner, manual intervention necessary";
-fi
+##check to make sure bzminer is running and installed
+##is bzminer configed and started?
+#miner status | grep -q bzminer
+#checkMiner=$?
+#if [[ checkMiner == 1 ]]; then
+#	echo "bzminer has not been started.  Make sure the flightsheet is configured to run bzminer 10.0.4 and start the miner ('miner start')";
+#fi
+#
+##is the executable installed and running
+#checkInstalled=1
+#if [ -f "$workDir/bzminer" ]; then
+#	ps -ef | grep -q "[0-9] $workDir/bzminer";
+#	checkInstalled=$?;
+#fi
+#if [[ $checkInstalled == 1 ]]; then
+#	echo "the proper version of bzminer is not installed and running, make sure the flightsheet is set to 10.0.4 and use 'miner log' to check the installation process";
+#fi
+#
+##does the log file show the miner has started?
+#grep -q "Working Directory: $workDir" /var/log/miner/bzminer/miner.log
+#checkRunning=$?
+#if [[ $checkRunning == 1 ]]; then
+#	echo "bzminer doesn't seem to be running";
+#fi
+#
+#if [[ $checkRunning == 1 || $checkInstalled == 1 || checkMiner == 1 ]]; then
+#	echo "Try again goofus :-)";
+#	exit 1;
+#fi
+#
+#echo "Initial checks good, proceeding"
+#
+##stop miner to prevent loosing mods to config.txt
+#if ! miner stop ; then
+#	echo "Failed to stop miner, manual intervention necessary";
+#fi
 
 #get params from config.txt
 cd $workDir
