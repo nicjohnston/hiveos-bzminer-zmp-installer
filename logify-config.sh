50e18316becfaee691b7c82b94a67f18a324ad1e
Nicholas Johnston <nicholasjohnstoncode@gmail.com>
2022-09-17 02:33:01 -0600

    make logify safer

--- logify-config.sh
+++ logify-config.sh
@@ -12,7 +12,18 @@ miner stop
 
 release-config.sh
 
-cat config.txt | jq '.pool = [0,1] | .+ {"log_file_verbosity":4}' | tee config.txt
+cat config.txt | jq '.pool = [0,1] | .+ {"log_file_verbosity":4}' > config-new.txt
+
+#inform user:
+echo "The following changes are about to be applied to config.txt:"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+diff -y config.txt config-new.txt
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo
+
+if [[ $1 != "y" ]]; then
+	read -p "Press enter to apply this change, Ctrl-C to cancel";
+fi
 
 protect-config.sh
 
